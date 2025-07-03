678bda6cb8543b0bd59c112ed077b7bffe0eef91
conor <conorsullivan182@gmail.com>
2025-07-03 15:07:26 -0400

    added king ability
    untested - hard to test yet

---
+++ resources/characters/abilities/king_ability.gd
@@ -0,0 +1,20 @@
+class_name KingAbility extends Ability
+
+
+func player_do_ability() -> void:
+	GameData.current_battle.real_player.is_king = true
+	GameData.current_battle.opponent_player.is_king = false
+
+	gain_gold_for_districts()
+
+func opponent_do_ability() -> void:
+	GameData.current_battle.real_player.is_king = false
+	GameData.current_battle.opponent_player.is_king = true
+
+	gain_gold_for_districts()
+
+
+func gain_gold_for_districts() -> void:
+	for d in GameData.current_battle.current_players_turn.district_cards_in_play:
+		if d.color == 'Gold':
+			GameData.current_battle.current_players_turn.gold_count += 1
